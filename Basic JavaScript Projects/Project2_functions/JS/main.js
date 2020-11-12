3fe0f554cd87609f22672c1a077f7bbcbf2ba0e4
kameron Jackson <kameronjackson28@gmail.com>
2020-11-12 14:29:58 -0600

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,2 +1,11 @@
-const p = document.getElementById('message');
-console.log(p);
+function My_First_Function() {
+    var str = "This text is green";
+    var result = str.fontcolor("Green");
+    document.getElementById("Green_Text") .innerHTML=result;
+}//This uses ElementbyId to make the button function text green//
+
+function myFunction() {
+    var sentence = "I am learning" ;
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}//This uses ElementbyId to make the words change when clicked//
