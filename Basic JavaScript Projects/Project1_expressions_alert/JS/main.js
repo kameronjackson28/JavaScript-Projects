612e1cbd426afd03ec1e3c6f6eaf8b5f93526206
kameron Jackson <kameronjackson28@gmail.com>
2020-11-12 11:48:47 -0600

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello, world");
+document.write("\"Dream as if you'll live forever, live as if you'll die today.\""
++"-James Dean");
+
+
+
+var Family ='<pre>"The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"</pre>';
+var Family =Family.fontcolor("green");//Defining a string variable and assigning it's color//
+document.write(Family);
+
+document.write(3 + 3);//Creating an expression//
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
