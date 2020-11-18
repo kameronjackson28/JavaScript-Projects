6ab41b83b4a9933e44992ca5d27a70a5955e5792
kameron Jackson <kameronjackson28@gmail.com>
2020-11-17 22:33:28 -0600

    Update main.js

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -9,22 +9,61 @@ Add_numbers_1();
 Add_numbers_2();//above function uses global scope to display 30 and 110//
 
 function Add_numbers_3() {
-    var X = 10;
-    document.write(20 + X + "<br>");
+    var Z = 30;
+    document.write(20 + Z + "<br>");
 }
 
 function Add_number_4() {
-    document.write(X + 100);
+    document.write(Z + 100);
     }
 Add_numbers_3();
 Add_number_4();//above function uses local scope to display 30//
 
 function Add_numbers_5() {
-    var X = 10;
-    console.log(15 + X);
+    var y= 20;
+    console.log(15 + Y);
 }
 function Add_numbers_6 () {
-    console.log(X + 100);
+    console.log(Y + 100);
 }
 Add_numbers_5();
-Add_numbers_6();
+Add_numbers_6();//uses console.log to debug issues//
+
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}//uses if statement to return greeting before 6p local time//
+
+function get_Greeting() {
+    if (new Date().getHours() < 24) {
+        document.getElementById("Hello").innerHTML= "time to get to bed!";
+    }
+}//uses if statement to again return greeting if before midnight local time//
+
+function Age_function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vape ="You can buy vape products!";
+    }
+    else {
+        Vape ="You cannot buy vape products!";
+    }
+    document.getElementById("age_by_number").innerHTML= Vape;
+}//uses if else statement to calculate whether user can buy vape products or not//
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is evening time.";
+    }
+    else {
+        Reply ="It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}//Uses else if statement to pull time of day and create return based on that time//
+
