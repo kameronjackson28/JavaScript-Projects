43fd5899ce78d3abb9a9c86724ea3ce32ac091db
kameron Jackson <kameronjackson28@gmail.com>
2020-11-17 19:01:26 -0600

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -29,3 +29,13 @@ document.write(10==10);//Creates an output of true since values are equal//
 DateOfBirth1 = 1/2/1985;//Data used for the 3 equals//
 DateOfBirth2 = 1/3/1986;//Data used for the 3 equals//
 document.write(DateOfBirth1 === DateOfBirth2);//Puts an output of false since the values aren't the same//
+document.write(5 > 2 && 10 > 4);//Uses Boolean logical operator AND to determine the logic between two variables//
+document.write(5 > 10 && 10 > 4); //Uses Boolean logical operator AND to return false//
+document.write(5 > 10 || 10 > 4);//Uses Boolean logical operator OR to return true based on the values//
+document.write(5 > 10 || 10> 20);//Uses Boolean logical operator OR to return false based on the values//
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}//Uses Boolean logical operator NOT to return false//
+function true_function() {
+    document.getElementById("true").innerHTML = !(5 > 10);
+}//Uses Boolean logical operator NOT to return true//
